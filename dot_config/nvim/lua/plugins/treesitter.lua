3862fdcbd808abf57057591e53a5681fc0341c94
Sander Boelhouwers <3847279+ScaredStorm@users.noreply.github.com>
2026-04-24 20:35:46 +0200

    fix: remove old plugin files

--- dot_config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        require"nvim-treesitter".install {"rust", "javascript", "typescript", "c", "cpp", "markdown", "typst" }
-    end
-}
