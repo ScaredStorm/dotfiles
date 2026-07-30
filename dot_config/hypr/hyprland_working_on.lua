81dcbf4cb30a47be479ec1fb8ed68472b66103c5
Sander Boelhouwers <3847279+ScaredStorm@users.noreply.github.com>
2026-07-30 16:14:48 +0200

    feat(hypr): moving hypr config to lua

---
+++ dot_config/hypr/hyprland_working_on.lua
@@ -0,0 +1,12 @@
+require("system.variables")
+require("system.exec")
+require("system.global")
+
+require("modules.animations")
+require("modules.decoration")
+require("modules.colors")
+require("modules.input")
+require("modules.gestures")
+require("modules.keybinds")
+require("modules.rules")
+
